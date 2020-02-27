c5254e031b6642b8b8952d5a59df9e5d30fd9312
ThatcherK <kaliithatmau@gmail.com>
2020-02-27 05:32:26 -0800

    Add first page

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export default function SignIn() {
-    return (
-        <div>
-            
+	return (
+        <div className="signInContainer">
+            <form>
+			<label for="name"> Name:</label><br/>
+			<input type="text" name="name" placeholder="Name"/><br/><br/>
+			<label for="email">Email address:</label><br/>
+			<input type="text" name="email" placeholder="Email" /><br/><br/>
+			<label for="Password">Password:</label><br/>
+			<input type="password" name="password" placeholder="password" /><br/><br/>
+			<input type="submit" value="Submit" />
+		</form>
         </div>
-    )
+		
+	);
 }
