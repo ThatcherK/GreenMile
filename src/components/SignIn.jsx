c2f2629ad6467e724f17d75052495ba8a6120906
ThatcherK <kaliithatmau@gmail.com>
2020-02-27 05:55:02 -0800

    Add style

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default function SignIn() {
 	return (
         <div className="signInContainer">
-            <form>
-			<label for="name"> Name:</label><br/>
-			<input type="text" name="name" placeholder="Name"/><br/><br/>
-			<label for="email">Email address:</label><br/>
-			<input type="text" name="email" placeholder="Email" /><br/><br/>
-			<label for="Password">Password:</label><br/>
-			<input type="password" name="password" placeholder="password" /><br/><br/>
-			<input type="submit" value="Submit" />
+            <form className="signInForm">
+			<label for="name"> Name</label>
+			<input type="text" name="name" placeholder="Name"/>
+			<label for="email">Email address</label>
+			<input type="text" name="email" placeholder="Email" />
+			<label for="Password">Password</label>
+			<input type="password" name="password" placeholder="password" /><br/>
+			<input type="submit" value="Log In" />
 		</form>
         </div>
 		
