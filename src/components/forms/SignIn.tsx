419faaab26ac9c34dabfb882b5e2683bd9d2550c
ThatcherK <kaliithatmau@gmail.com>
2021-02-12 14:40:06 +0300

    remove types from signin component

--- src/components/forms/SignIn.tsx
+++ src/components/forms/SignIn.tsx
@@ -3,62 +3,77 @@ import { authContext } from '../context/Authenticate';
 import instance from '../config/axiosConfig';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import { string } from 'yup/lib/locale';
 
-interface IFormValues{
-  email: string,
-  password: string
-}
-const  SignIn:FunctionComponent = (props)=> {
-	const { setLogIn, setToken } = useContext(authContext);
+const SignIn: FunctionComponent = (props) => {
+  const { setLogIn, setToken } = useContext(authContext);
 
-	const formik = useFormik({
-		initialValues : {
-			email: '',
-			password: '',
-		},
-		validationSchema: Yup.object<>({
-			email: Yup.string().email('Wrong email format').required('Required'),
-			password: Yup.string().min(8, 'Minimum 8 characters').required('Required!'),
-		}),
-		onSubmit: (values, onSubmitProps) => {
-			handleLogin();
-			onSubmitProps.resetForm()
-		}
-	});
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object.I({
+      email: Yup.string().email('Wrong email format').required('Required'),
+      password: Yup.string()
+        .min(8, 'Minimum 8 characters')
+        .required('Required!'),
+    }),
+    onSubmit: (values, onSubmitProps) => {
+      handleLogin();
+      onSubmitProps.resetForm();
+    },
+  });
 
-	const payload = { 'email': formik.values.email, 'password': formik.values.password }
+  const payload = {
+    email: formik.values.email,
+    password: formik.values.password,
+  };
 
-	const handleLogin = () => {
-		instance.post('http://127.0.0.1:5000/login', payload)
-			.then((response) => {
-				setLogIn(true)
-				setToken(response.data.auth_token)
-				localStorage.setItem('token', response.data.auth_token)
-				localStorage.setItem('role', response.data.user.role)
-			})
-			.catch((err) => {
-				return err
-			});
-	};
-	return (
-		<div className="signInContainer">
-			<form className="signInForm" onSubmit={formik.handleSubmit}>
-				<input type="text" name="email" value={formik.values.email} placeholder="Email" onChange={formik.handleChange} />
+  const handleLogin = () => {
+    instance
+      .post('http://127.0.0.1:5000/login', payload)
+      .then((response) => {
+        setLogIn(true);
+        setToken(response.data.auth_token);
+        localStorage.setItem('token', response.data.auth_token);
+        localStorage.setItem('role', response.data.user.role);
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+  return (
+    <div className="signInContainer">
+      <form className="signInForm" onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          value={formik.values.email}
+          placeholder="Email"
+          onChange={formik.handleChange}
+        />
 
-				<input type="password" name="password" value={formik.values.password} placeholder="Password" onChange={formik.handleChange} />
+        <input
+          type="password"
+          name="password"
+          value={formik.values.password}
+          placeholder="Password"
+          onChange={formik.handleChange}
+        />
 
-				<br />
-				<button className="authBtn" type="submit" onClick={handleLogin}>
-					Log In
-				</button>
-				<br />
-				<label>Not yet a user?</label>
-				<br />
-				<button className="signUpBtn" onClick={props.showSignUp}>
-					Sign Up
-				</button>
-			</form>
-		</div>
-	);
-}
+        <br />
+        <button className="authBtn" type="submit" onClick={handleLogin}>
+          Log In
+        </button>
+        <br />
+        <label>Not yet a user?</label>
+        <br />
+        <button className="signUpBtn" onClick={props.showSignUp}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
